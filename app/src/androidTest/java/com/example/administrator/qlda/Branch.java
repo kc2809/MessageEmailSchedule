50c1d43d7ed6fe28612ddf6f084a06dcfb6e0489
khanh nguyen <zhuukhanhz@gmail.com>
2016-12-07 08:45:30 +0700

    branch01: add new file

---
+++ app/src/androidTest/java/com/example/administrator/qlda/Branch.java
@@ -0,0 +1,12 @@
+package com.example.administrator.qlda;
+
+import android.util.Log;
+
+/**
+ * Created by GIAKHANH on 12/7/2016.
+ */
+public class Branch {
+    public void xinchao(){
+        System.out.println("xin chao viet nam");
+    }
+}
